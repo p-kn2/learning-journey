79c4aea7e62baba5645bb4bdf521ff42b2175758
Ubuntu <ubuntu@ip-172-31-9-144.ec2.internal>
2026-08-06 08:55:22 +0000

    tfcommit

---
+++ s3.tf
@@ -0,0 +1,9 @@
+# This bucket should be created before running terraform apply.
+
+# resource "S3" "learning-journey-terraform-state" {
+#     bucket = "learning-journey-terraform-state"
+
+#     tags = {
+#         Name = "learning-journey-terraform-state"
+#     }
+# }
