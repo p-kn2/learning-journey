b69485ab964dde47d415c98cdd9f11c35e5e4b35
Ubuntu <ubuntu@ip-172-31-9-144.ec2.internal>
2026-08-06 08:36:10 +0000

    fourth cmt

---
+++ dev.tfvars
@@ -0,0 +1,4 @@
+aws_region = "us-east-1"
+ami_id = "ami-0b6d9d3d33ba97d99"
+instance_type = "t3.micro"
+instance_name = "First_instance"
--- variables.tf
+++ variables.tf
@@ -12,3 +12,8 @@ variable "instance_type" {
     description = "The instance type for the EC2 instance"
     type = string
 }
+
+variable "instance_name" {
+    description = "The name tag for the EC2 instance"
+    type = string
+}
